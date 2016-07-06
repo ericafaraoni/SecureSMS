fd9b8368ca4360854a408f35ed76451690610cb0
ericafaraoni <e.faraoni@hotmail.it>
2016-07-06 19:58:54 +0200

    Menu added

--- app/src/main/java/com/ssms/securesms/MainActivity.java
+++ app/src/main/java/com/ssms/securesms/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -32,5 +34,25 @@ public class MainActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.MENU_1) {
+            /*
+                Codice di gestione della voce MENU_1
+             */
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
